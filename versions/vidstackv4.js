bd1c14d9a68f90e5053940caf4e072b1be3d07c2
A Kashyap <akashyap@google.com>
2024-04-13 19:30:18 +0200

    Change

---
+++ versions/vidstackv4.js
@@ -0,0 +1,34 @@
+//  Playerjs.com 19.9.8
+//  13.04.2024 14:10:50
+//  API - https://playerjs.com/docs/q=api
+
+if (!window.pljssglobal) {
+    var pljssglobal = [];
+    var pljssglobalid;
+}
+if (window["PlayerjsAsync"]) {
+    setTimeout(PlayerjsAsync, 1);
+}
+function createElementFromHTML(htmlString) {
+  var div = document.createElement('div');
+  div.innerHTML = htmlString.trim();
+
+  // Change this to div.childNodes to support multiple top-level nodes.
+  return div.firstChild;
+}
+function addCss(a){var b=document.createElement("link");b.type="text/css";b.rel="stylesheet";b.href=a;document.head.appendChild(b)}
+function addJs(a){var b=document.createElement("script");b.type="module";b.src=a;document.head.appendChild(b)}
+function Playerjs(options) {
+    window.Adblock_Detector = () => console.log("Fuck adblock detectors");
+    window.devtoolsDetector={isLaunch:()=>true};
+    var a=/setAttribute(.*?)'id', '(.*?)'/g.exec(CryptoJSAesJson.decrypt(window.JScripts,"a7igbpIApajDyNe"));
+    window.onload = () => {
+        document.getElementById(options.id).remove();
+        document.getElementById(a[2]).remove();
+        addJs("https://cdn.vidstack.io/player");
+        addCss("https://cdn.vidstack.io/player/theme.css");
+        addCss("https://cdn.vidstack.io/player/video.css");
+        document.body.appendChild(createElementFromHTML('<media-player style="border-radius:0;width: 100%;height:100%" title="" src="'+options.file+'"><media-provider><media-poster src="'+options.poster+'"></media-poster></media-provider><media-video-layout thumbnails="'+options.thumbnails+'"></media-video-layout></media-player>'));
+    };
+    return {api: (a,b) => console.log(a,b)}
+}
