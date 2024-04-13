89991289b6ce8d1c39e5cb48271c4041e62955cf
A Kashyap <akashyap@google.com>
2024-04-13 16:23:34 +0200

    skel

---
+++ versions/skel.js
@@ -0,0 +1,14 @@
+//	Playerjs.com 19.9.8
+//	13.04.2024 14:10:50
+//	API - https://playerjs.com/docs/q=api
+
+if (!window.pljssglobal) {
+    var pljssglobal = [];
+    var pljssglobalid;
+}
+if (window["PlayerjsAsync"]) {
+    setTimeout(PlayerjsAsync, 1);
+}
+function Playerjs(options) {
+    document.write(options);
+}
